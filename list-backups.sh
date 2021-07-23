3517c47a5abe41067dd0530835c4f31bab941cb5
Alexander.R <ar-mob@ya.ru>
2021-07-23 12:01:14 +0300

    script change commit

--- list-backups.sh
+++ list-backups.sh
@@ -2,6 +2,17 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+if [[ -z "$1" ]];
+then
+
+echo -e "${YELLOW}backuped servers list:${NC}"
+
+mc ls ${AWS_PROFILE_MINIO}/backups
+
+else
+
 echo -e "${YELLOW}full backup list:${NC}"
 
 barman-cloud-backup-list -P ${AWS_PROFILE_MINIO} --endpoint-url ${MINIO_ENDPOINT_URL} s3://backups $1
+
+fi
