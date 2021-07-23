efdbdce4ec8d04a8a8e9076577c075b84d175da5
Alexander.R <ar-mob@ya.ru>
2021-07-23 11:51:53 +0300

    add minio mc commit

--- s3-entrypoint.sh
+++ s3-entrypoint.sh
@@ -2,6 +2,7 @@
 
 aws configure --profile ${AWS_PROFILE_MINIO:-minio} set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
 aws configure --profile ${AWS_PROFILE_MINIO:-minio} set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
+mc config --quiet host add ${AWS_PROFILE_MINIO} ${MINIO_ENDPOINT_URL} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY}
 
 #cat /root/.aws/credentials 
 #cat /var/lib/postgresql/.aws/credentials 
